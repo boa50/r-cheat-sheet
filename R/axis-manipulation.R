ad8eec560c850a58cbe04126f5ea67d55947c4bc
boa50 <bruno.albuquerque50@gmail.com>
2023-03-25 03:51:15 +1030

    Added more information about axis manipulation

--- R/axis-manipulation.R
+++ R/axis-manipulation.R
@@ -11,8 +11,10 @@ df <- data.frame(x, y)
 
 ggplot(df, aes(x, y)) +
   geom_line() +
+  labs(x = "New x label", y = "New y label") +
   scale_x_continuous(breaks = seq(0, 10, by = 2),
                      expand = expansion(mult = c(.02, .02))) +
   scale_y_continuous(breaks = seq(0, 15, by = 3),
                      limits = c(0, 15),
-                     expand = expansion(mult = 0))
+                     expand = expansion(mult = 0),
+                     labels = scales::label_number(suffix = "m\u00B2"))
